7fcf589439861acaa46813cb455805de33592530
Jeremiah dufourq <jezdufourq@gmail.com>
2021-01-04 22:34:21 +1000

    update: card component for the github projects.

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -8,22 +8,32 @@ export default class Cards extends Component {
         id="greeting"
       >
         <h1 className="w-full text-5xl font-serif text-bold text-black tracking-tight">
-          This is a title
+          Github Projects
         </h1>
-        <div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
-          <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
-            <a href="#" class="flex flex-wrap no-underline hover:no-underline">
-              <p class="w-full text-gray-600 text-xs md:text-sm px-6">
-                GETTING STARTED
-              </p>
-              <div class="w-full font-bold text-xl text-gray-800 px-6">
-                Lorem ipsum dolor sit amet.
+        <div className="container mx-auto flex flex-wrap pt-4 pb-12">
+          <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+            <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+              <a
+                href="#"
+                className="flex flex-wrap no-underline hover:no-underline"
+              >
+                <p className="w-full text-gray-600 text-xs md:text-sm px-6">
+                  xGETTING STARTED
+                </p>
+                <div className="w-full font-bold text-xl text-gray-800 px-6">
+                  Lorem ipsum dolor sit amet.
+                </div>
+                <p className="text-gray-800 text-base px-6 mb-5">
+                  className ipsum dolor sit amet, consectetur adipiscing elit.
+                  Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.
+                </p>
+              </a>
+              <div className="flex items-center justify-center">
+                <button className="mx-auto lg:mx-0 hover:underline bg-blue-500 text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                  Action
+                </button>
               </div>
-              <p class="text-gray-800 text-base px-6 mb-5">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-                at ipsum eu nunc commodo posuere et sit amet ligula.
-              </p>
-            </a>
+            </div>
           </div>
         </div>
       </div>
