a61978a484abcbd738088f170d9b97b10818465d
jeremiah dufourq <jezdufourq@gmail.com>
2021-01-18 21:14:15 +1000

    add: styling to the welcome page.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -21,11 +21,11 @@ function IndexPage() {
       />
       <Header />
       <div className="flex flex-wrap overflow-hidden">
-        <div className="w-full py-20 pl-14 pr-4 overflow-hidden content-center md:w-1/2">
-          <div className="col-auto py-4 align-center text-right justify-right">
+        <div className="grid grid-rows-2 place-content-center gap-3 md:w-1/2">
+          <div className="col-auto text-right self-end">
             <Title string="Welcome" />
           </div>
-          <div className="py-2 ml-20 text-right">
+          <div className="col-auto ml-20 text-right">
             <span className="font-bold font-sans">
               My name is Jeremiah, and I am a full stack developer
             </span>
