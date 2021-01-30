b29e7b1916940aa24bbad7f202ef821d560bd143
jeremiah dufourq <jezdufourq@gmail.com>
2021-01-30 16:20:21 +1000

    add: context to index.html page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import React from "react"
-import Index from "../layouts"
+import React from "react";
+import Index from "../layouts";
 import SEO from "../components/seo";
 import Title from "../components/title/title";
 import DisplayLottie from "../components/DisplayLottie";
@@ -21,24 +21,26 @@ export default function Home() {
         title="Home"
       />
       <div className="flex flex-wrap overflow-hidden">
-        <div className="grid grid-rows-2 pr-4 gap-3 w-full md:w-1/2">
-          <div className="col-auto text-right self-end">
+        <div className="h-100 w-full grid grid-rows-2 gap-3 md:pr-4 md:w-1/2">
+          <div className="col-auto self-end text-center md:text-right">
             <Title string="Welcome" />
           </div>
-          <div className="col-auto ml-20 text-right">
+          <div className="col-auto text-center md:ml-20 md:text-right">
             <span className="font-bold font-sans">
               My name is Jeremiah, and I am a full stack developer
             </span>
           </div>
         </div>
-        <div className="py-20 pr-14 pl-4 overflow-hidden w-full md:w-1/2">
-          <DisplayLottie className="w-30 h-30" animationData={landingPerson} />
+        <div className="w-full md:py-20 md:pr-14 md:pl-4  md:w-1/2">
+          <DisplayLottie animationData={landingPerson} />
         </div>
       </div>
       <div className="flex flex-wrap overflow-hidden">
         <div className="grid grid-rows-1 pr-4 w-full md:w-1/2">
-          <div className="col-auto justify-right place-items-end self-center">
-            <div className="w-full h-full"></div>
+          <div className="col-auto self-center text-center md:ml-20 md:text-right">
+            <span className="font-bold font-sans">
+              Image goes here
+            </span>
           </div>
         </div>
         <div className="grid grid-rows-2 pl-4 gap-3 w-full md:w-1/2">
@@ -63,17 +65,21 @@ export default function Home() {
         </div>
       </div>
       <div className="flex flex-wrap overflow-hidden">
-        <div className="mx-80 my-10 grid grid-rows-1 w-full text-center text-4xl text-primary font-title">
+        <div className="mx-80 my-10 md:mx-40 grid grid-rows-1 w-full text-center text-4xl text-primary font-title">
           I love creating solutions to everyday problems.
         </div>
       </div>
       <div className="flex flex-wrap overflow-hidden">
-        <div className="mx-80 my-10 grid grid-rows-1 w-full text-center text-4xl text-primary font-title">
+        <div className="mx-80 my-10 md:mx-40 grid grid-rows-1 w-full text-center text-4xl text-primary font-title">
           Ready to take the next step and work together?
         </div>
-        <div className="mx-40 grid grid-rows-1 w-full text-center text-4xl text-black font-title">
+      </div>
+      <div className="flex flex-wrap overflow-hidden">
+        <div className="mx-80 grid grid-rows-1 w-full text-center text-4xl font-title md:w-1/2">
           Let's have a chat
         </div>
+      </div>
+      <div className="flex flex-wrap overflow-hidden">
         <div className="grid grid-rows-1 w-full justify-items-center">
           <Button name="Contact Me" />
         </div>
