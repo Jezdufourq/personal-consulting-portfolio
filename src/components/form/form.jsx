4d2d4294f0b7f2619e31b102998db055880adb60
jeremiah dufourq <jezdufourq@gmail.com>
2021-01-31 16:10:06 +1000

    add: custom form without js functions.

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -3,40 +3,26 @@ import { navigate } from "gatsby";
 import "./form.css";
 
 export default function form() {
-  function encode(data) {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&");
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": event.target.getAttribute("name"),
-        ...name,
-      }),
-    })
-      .then(() => navigate("/success/"))
-      .catch((error) => alert(error));
-  };
   return (
-    <form
-      data-netlify="true"
-      name="pizzaOrder"
-      method="post"
-      onSubmit={handleSubmit}
-    >
-      <input type="hidden" name="form-name" value="pizzaOrder" />
-      <label>
-        What order did the pizza give to the pineapple?
-        <input name="order" type="text" />
-      </label>
-      <input type="submit" />
+    <form name="contact" method="POST" data-netlify="true" action="/success">
+      <p>
+        <label>
+          Your Name: <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email: <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message: <textarea name="message"></textarea>
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
     </form>
   );
 }
