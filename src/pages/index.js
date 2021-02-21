d3520a9556e7d5d2d625df6f38f487092d5afe6d
Jeremiah dufourq <jezdufourq@gmail.com>
2021-02-21 19:51:54 +1000

    update: cleaned up the index.js file.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,8 @@
 import React from "react";
 import Index from "../layouts";
 import SEO from "../components/seo";
-import Title from "../components/title/title";
-import DisplayLottie from "../components/lottie/DisplayLottie";
-import landingPerson from "../assets/lottie/landingPerson.json";
-import TagCloud from "../components/TagCloud/TagCloud";
-import Button from "../components/button/button";
-import Avatar from "../components/avatar/avatar";
-import jeremiahAvatar from "../assets/jeremiah.jpeg";
-import { navigate } from "gatsby";
-import { skills } from "../source";
 import { Fade } from "react-reveal";
-import Welcome from "../components/welcomeSection/welcome";
+import Welcome from "../components/welcomeSection/Welcome";
 import AboutMe from "../components/aboutMeSection/AboutMe";
 import MySkills from "../components/mySkillsSection/MySkills";
 import BottomSection from "../components/bottomSection/BottomSection";
@@ -19,10 +10,6 @@ import ExperienceSection from "../components/experienceSection/ExperienceSection
 import ProjectSection from "../components/projectSection/ProjectSection";
 
 export default function Home() {
-  function contactPage() {
-    navigate("/contact");
-  }
-
   return (
     <Index>
       <Fade duration={2000} distance="200px">
