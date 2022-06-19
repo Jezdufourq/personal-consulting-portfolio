abc1a0d2518f476f20cd1858058a3dfd4235e52e
Jeremiah dufourq <jezdufourq@gmail.com>
2022-06-19 15:02:17 +1000

    chore: updated my skills section.

--- src/components/mySkillsSection/MySkills.jsx
+++ src/components/mySkillsSection/MySkills.jsx
@@ -23,10 +23,10 @@ export default function MySkills() {
           </div>
           <div className="flex-auto col-auto text-center md:ml-20 md:text-right">
             <span className="font-bold font-sans">
-              I am proficient in Typescript, Java, and SQL, and have had
-              experience in Vue and React. I have also completed projects which
-              use Docker, Terraform, Kubernetes, Python and C++. My interests
-              lie within Fintech, Web Applications, Cloud Architectures.
+              I am proficient in Typescript, Java, and SQL with experience in
+              Vue and React. I have also completed projects which use Docker,
+              Terraform, Kubernetes, Python and C++. My interests lie within
+              Fintech, Web Applications, Cloud Architectures.
             </span>
           </div>
         </div>
